ae325b07eb220caf1e931cf8175c9bbb2dd2e5dd
Said Maadan <me@saidmaadan.com>
2017-12-04 08:49:03 -0600

    Implemented pretty_url with friendly_id

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,13 @@
 class Project < ApplicationRecord
-    has_many :tasks
+  extend FriendlyId
+  friendly_id :name, use: [:slugged, :finders]
+  
+  has_many :tasks
 
-    validates :name, presence: true, length: { maximum: 50}
-    validates :content, presence: true, length: { maximum: 500}
-    validates :price, presence: true, numericality: { only_integer: true }
+  validates :name, presence: true, length: { maximum: 50}
+  validates :content, presence: true, length: { maximum: 500}
+  validates :price, presence: true, numericality: { only_integer: true }
 
-    has_attached_file :image, styles: { medium: "680x300>", thumb: "170x75>" }
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  has_attached_file :image, styles: { medium: "680x300>", thumb: "170x75>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
