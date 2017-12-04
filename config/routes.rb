f6fd5a87926246794e6aaf6b37b333fad8bcab17
Said Maadan <me@saidmaadan.com>
2017-12-03 20:51:43 -0600

    Created project and task views-controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
               :controllers => {:omniauth_callbacks => 'omniauth_callbacks'}
   get 'pages/about'
 
-  root to: 'pages#about'
+  root to: 'project#index'
+
+  resources :project do
+    resources :task, only: [:show]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
